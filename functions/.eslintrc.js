f903cda84c4e81c2ee51f6401db67ed57da70fe3
radall <radall@outlook.com>
2024-06-17 17:28:31 +0000

    fix eslint config

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   root: true,
   env: {
     es6: true,
@@ -14,7 +14,7 @@ module.exports = {
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: ["functions/tsconfig.json", "functions/tsconfig.dev.json"],
+    project: ["tsconfig.json", "tsconfig.dev.json"],
     sourceType: "module",
   },
   ignorePatterns: [
